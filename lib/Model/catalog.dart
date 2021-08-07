a7471fa6581e232183f282d848103a5349f7788a
amitkum8227 <62775823+amitkum8227@users.noreply.github.com>
2021-08-06 21:32:51 -0400

    "displying products from json file"

--- lib/Model/catalog.dart
+++ lib/Model/catalog.dart
@@ -1,14 +1,15 @@
 class CatalogModel {
-  static final items = [
-    Item(id:1,
+  static List<Item> items = [
+    Item(
+        id: 1,
         name: "Iphone 12 pro",
         desc: "Apple phone 12 pro",
         price: 999,
         color: '#33505a',
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
   ];
 }
-
 
 class Item {
   final int id;
@@ -18,6 +19,32 @@ class Item {
   final String color;
   final String image;
 
-  Item(
-      {required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+  Item({required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image});
+
+  factory Item.fromMap(Map<String, dynamic>map){
+    return Item(id: map["id"],
+        name: map["name"],
+        desc: map["desc"],
+        price: map["price"],
+        color: map["color"],
+        image: map["image"]);
+  }
+
+  toMap()=>{
+    "id":id,
+    "name":name,
+    "desc":desc,
+    "price":price,
+    "color":color,
+    "image":image,
+
+
+
+
+  };
 }
--- lib/Screen/home_page.dart
+++ lib/Screen/home_page.dart
@@ -1,9 +1,35 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/Model/catalog.dart';
 import 'package:flutter_catalog/Widget/drawer.dart';
 import 'package:flutter_catalog/Widget/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState()  {
+    super.initState();
+    loadData();
+  }
+  loadData() async{
+    final catalogJson= await rootBundle.loadString("assets/files/catalog.json");
+
+    final decodeData=jsonDecode(catalogJson);
+
+    var productsData=decodeData["products"];
+    CatalogModel.items=List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+    //print(productsData);
+    setState(() {
+
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
